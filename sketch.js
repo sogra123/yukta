a2331795702e7cca3f6e947064bda887071da98a
sogra123 <farheemwh@gmail.com>
2021-03-28 13:23:44 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    platform = new Ground(200,150,10,10);
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -53,7 +53,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
-
+platform.display();
     box5.display();
     log4.display();
     log5.display();
